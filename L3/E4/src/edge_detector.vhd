7549d2c76cccdc66de5c3a406d00bf25721329eb
mab <schoch1995@gmail.com>
2022-04-11 15:07:24 +0200

    L3 comment, format, removed obsolete files & changed .vhd includes in .tex file

--- L3/E4/src/edge_detector.vhd
+++ L3/E4/src/edge_detector.vhd
@@ -1,45 +1,39 @@
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_1164.all;
 
 entity edge_detector is
-    Port ( 
-        CLK : in std_logic; 
+    port (
+        CLK : in std_logic;
         I : in std_logic;
         EDGE : out std_logic
     );
 end edge_detector;
 
 architecture Behavioral of edge_detector is
-signal edge_out : std_logic := '0';
-signal was_zero : std_logic := '0';
-signal timer_T : std_logic;
+    signal edge_out : std_logic := '0';
+    signal was_zero : std_logic := '0';
+    signal timer_T : std_logic;
 begin
     freq_changer : entity work.project_3_1
-    generic map (
-        FREQ_IN => 100e6,
-        FREQ_OUT => 100
-    )
-    port map (
-        CLK => CLK,
-        T => timer_T
-    );
-    
+        generic map(
+            FREQ_IN => 100e6,
+            FREQ_OUT => 100 -- 100Hz to eliminate bouncing effect
+        )
+        port map(
+            CLK => CLK,
+            T => timer_T
+        );
+
     process (CLK) begin
         if rising_edge(CLK) then
-            edge_out <= '0';
-            if timer_T = '1' then
-                --if I = '0' then
-                    --was_zero <= '1';
-                --else
-                    was_zero <= I;
-                --end if;
-                
-                if I = '1' and was_zero = '0' then
-                    edge_out <= '1';
+            edge_out <= '0'; -- (*)
+            if timer_T = '1' then -- 
+                was_zero <= I; -- set I to was_zero, but only in the next CLK cycle because its a signal
+                if I = '1' and was_zero = '0' then -- was_zero is I from previous CLK cycle, I is always current
+                    edge_out <= '1'; -- goes to '1' for 1 CLK cycle (*)
                 end if;
-                
             end if;
         end if;
     end process;
-EDGE <= edge_out;
+    EDGE <= edge_out;
 end Behavioral;
