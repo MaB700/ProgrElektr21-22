27f8f0a543170dbefa379db7eb8aa5122a5c35db
JanLite <jan.leithaeuser@physik.uni-giessen.de>
2022-04-22 15:16:06 +0200

    fixed parity generator

--- L1/E3/src/project_1_3.vhd
+++ L1/E3/src/project_1_3.vhd
@@ -5,13 +5,16 @@ entity project_1_3 is
     port (
         sw : in std_logic_vector (15 downto 0);
         led : out std_logic_vector (1 downto 0) -- led(0) even , led(1) odd
-        -- P_EVEN : out std_logic;
-        -- P_ODD : out std_logic
+        P_EVEN : out std_logic;
+        P_ODD : out std_logic
     );
 end project_1_3;
 
 architecture Behavioral of project_1_3 is
+signal parity : std_logic;
 begin
-    led(1) <= sw(0) xor sw(1) xor sw(2) xor sw(3) xor sw(4) xor sw(5) xor sw(6) xor sw(7) xor sw(8) xor sw(9) xor sw(10) xor sw(11) xor sw(12) xor sw(13) xor sw(14) xor sw(15);
-    led(0) <= not (sw(0) xor sw(1) xor sw(2) xor sw(3) xor sw(4) xor sw(5) xor sw(6) xor sw(7) xor sw(8) xor sw(9) xor sw(10) xor sw(11) xor sw(12) xor sw(13) xor sw(14) xor sw(15));
+    parity <= sw(0) xor sw(1) xor sw(2) xor sw(3) xor sw(4) xor sw(5) xor sw(6) xor sw(7) xor sw(8) xor sw(9) xor sw(10) xor sw(11) xor sw(12) xor sw(13) xor sw(14) xor sw(15);
+    led <= parity;
+    P_EVEN <= parity;
+    P_ODD <= not parity;
 end Behavioral;
